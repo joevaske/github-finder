f7229aecf0b52cd43e9859d656e3d6a5bc6e4c1b
Djordje Vasic <djvasic@domain.local>
2021-03-24 12:47:55 +0100

    user component and repo display

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1> About this app </h1>
+            <p>App to search GitHub users</p>
+            <p>Version 1.1</p>
+        </Fragment>
+    )
+}
